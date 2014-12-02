8f12f0f37a58905a23b3bc9a8c9a539ad0c3b371
jkpark <jkpark3070@gmail.com>
2014-12-02 09:35:40 +0900

    20141202-0942

---
+++ Day1127/src/net/hb/bit/KaupIndex.java
@@ -0,0 +1,5 @@
+package net.hb.bit;
+
+public class KaupIndex {
+	double height;
+}
---
+++ Day1127/src/net/hb/controller/Main1202.java
@@ -0,0 +1,15 @@
+package net.hb.controller;
+
+import net.hb.model.Juso;
+
+public class Main1202 {
+	public static void main(String[] args) {
+		Main1202 m = new Main1202();
+		m.arrayTest();
+	}
+
+	public void arrayTest() {
+		Juso js = new Juso();
+		js.arrayTest();
+	}
+}
